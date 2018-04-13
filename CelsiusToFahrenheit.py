4e9a9f16fbd4cf14de52477a3d4991f2d19c3db2
hundredvisionsguy <winikkc@hsd.k12.or.us>
2018-04-13 16:06:16 -0700

    adding initial files

---
+++ CelsiusToFahrenheit.py
@@ -0,0 +1,35 @@
+# CelsiusToFahrenheit.py
+# By ____
+# TODO: write a function titled, celsiusToFahrenheit() that...
+#       receives a floating point number (celsius)
+#       tries to
+#           convert celsius to a float
+#           calculate fahrenheit
+#       catches a ValueError if input cannot be converted to a floating point
+#           If so, it will return -9999
+#
+
+
+# You will want to test the function by running this document
+# WARNING: you must NOT change the function name, or you will fail the tests
+# NOTE: you are welcome to choose the variable names for your parameters
+#       (they will not affect the tests).
+
+
+
+##################################
+## DO NOT CHANGE THE CODE BELOW ##
+##################################
+
+if __name__ == '__main__':
+
+    # Test celsiusToFahrenheit() using known inputs with expected outputs
+    # Example: passing it 0 would return 32.0, 100 would return 212.0
+    print celsiusToFahrenheit("d")
+    print celsiusToFahrenheit("10")
+    # try to pass function at least one string like 'five' 
+    # Does it return -9999 without crashing?
+
+    # try to pass the function a number as a string (for example: '10')
+    # Does it return -12.222222222222221
+    
---
+++ test_CelsiusToFahrenheit.py
@@ -0,0 +1,74 @@
+# test_CelsiusToFahrenheit.py
+# Tests Temp Converter functions
+
+import unittest
+import CelsiusToFahrenheit
+import re
+
+class KnownValues(unittest.TestCase):
+    # Set up
+    def setUp(self):
+        # get text from script for regex tests
+        self.textfile = open('CelsiusToFahrenheit.py', 'r')
+        self.scripttext = self.textfile.read()
+    # Tear down
+    def tearDown(self):
+        # close the file
+        self.textfile.close()
+        
+    ################################################
+    # Test celsiusToFahrenheit()
+    ################################################
+    # For exception Handling
+    def test_celsiusToFahrenheit_forTypeError(self):
+        # Capture the results of the function
+        result = CelsiusToFahrenheit.celsiusToFahrenheit("str")
+        # Check for expected output
+        self.assertEqual(-9999, result)
+    def test_celsiusToFahrenheit_for10F_as_string(self):
+        # Capture the results of the function
+        result = CelsiusToFahrenheit.celsiusToFahrenheit("10")
+        # Check for expected output
+        self.assertEqual(-12.222222222222221, result)
+
+    # For expected output
+    def test_celsiusToFahrenheit_for0F(self):
+        # Capture the results of the function
+        result = CelsiusToFahrenheit.celsiusToFahrenheit(0)
+        # Check for expected output
+        self.assertEqual(32.0, result)
+
+    def test_celsiusToFahrenheit_for100F(self):
+        # Capture the results of the function
+        result = CelsiusToFahrenheit.celsiusToFahrenheit(100)
+        # Check for expected output
+        self.assertEqual(212.0, result)
+
+    def test_celsiusToFahrenheit_for55F(self):
+        # Capture the results of the function
+        result = CelsiusToFahrenheit.celsiusToFahrenheit(55)
+        # Check for expected output
+        self.assertEqual(131.0, result)
+        
+
+    def test_celsiusToFahrenheit_forMinus50F(self):
+        # Capture the results of the function
+        result = CelsiusToFahrenheit.celsiusToFahrenheit(-50)
+        # Check for expected output
+        self.assertEqual(-58.0, result)
+
+    # Test regex search for a pattern in script
+    def test_try_cmd_present(self):
+        matches = re.findall("try:", self.scripttext)
+        match = matches[0]
+        self.assertEquals("try:", match)
+        
+    def test_except_ValueError_present(self):
+        matches = re.findall("except ValueError:", self.scripttext)
+        match = matches[0]
+        self.assertEquals("except ValueError:", match)
+
+# Run the tests
+if __name__ == '__main__':
+    unittest.main()
+    
